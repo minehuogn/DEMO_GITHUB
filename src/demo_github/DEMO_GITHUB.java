2150b3d8e9e8e9e124545641b73e6a823ef1ee79
minehuogneee <mmaihuong1112@gmail.com>
2025-05-29 20:28:05 +0700

    update main.java
    them dong mo ta demo

--- src/demo_github/DEMO_GITHUB.java
+++ src/demo_github/DEMO_GITHUB.java
@@ -16,6 +16,7 @@ public class DEMO_GITHUB {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        System.out.println("Demo push tu B va pull ve may A");
     }
     
 }
